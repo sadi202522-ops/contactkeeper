75482f3c1e202895a509df67b226a69ff1e2d00d
sadi <sadi202522@gmail.com>
2025-09-01 23:06:23 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <ContactRound clas

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,7 +1,14 @@
-import { Contact2 } from "lucide-react";
+import Image from "next/image";
 
 export const Logo = () => (
   <div className="p-2 bg-primary/20 rounded-lg">
-    <Contact2 className="h-6 w-6 text-primary" />
+    <Image
+      src="https://picsum.photos/24/24"
+      alt="Logo"
+      width={24}
+      height={24}
+      className="rounded-sm"
+      data-ai-hint="Sheikh Hasina"
+    />
   </div>
 );
